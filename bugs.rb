170e6acc7a66a889fd6565d7329df9c623c746b3
Trent Blackburn <trentbl@amazon.com>
2018-11-14 16:20:10 -0500

    fix some buggy code

--- bugs.rb
+++ bugs.rb
@@ -7,17 +7,7 @@ class Person
   end
 
   ## TODO: Fix the misspelled method name below.
-  def frist_name
+  def first_name
     'Jane'
-  end
-
-  # Duplicate attr_readers.
-  # TODO: Remove these comments and the 2 methods below.
-  def name
-    @name
-  end
-
-  def email
-    @email
   end
 end
